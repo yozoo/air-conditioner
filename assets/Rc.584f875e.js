262068d72c19e197ea31127a704df1fe7477ca09
yozoo <yozoo@users.noreply.github.com>
2022-06-27 17:09:42 +0000

    deploy: b8f30937245eea5831fbb8cfd169c39a419e15cd

---
+++ assets/Rc.584f875e.js
@@ -0,0 +1,1 @@
+import{u as t,j as e,R as o}from"./index.a30ce573.js";const s=()=>(t(),e(o,{}));export{s as default};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/air-conditioner/sw.js', { scope: '/air-conditioner/' })})}
